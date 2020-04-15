afe18d27953199e42249c65796dbc3b8bcacf0c5
Vinay Sudani <vinaysudani9@gmail.com>
2020-04-15 21:35:44 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     var address = search.value
 
-    fetch('http://localhost:3000/weather?address=' + address).then((response) => {
+    fetch('/weather?address=' + address).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messgeOne.textContent = data.error;
